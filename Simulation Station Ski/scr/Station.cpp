264d53043099cb984997842bb90baa76d5fd7ca3
CynthiaMiaina <CynthiaMiaina@cynthia-miaina>
2017-04-17 11:31:24 +0200

    creation classe station

---
+++ Simulation Station Ski/scr/Station.cpp
@@ -0,0 +1,21 @@
+/*
+ * Station.cpp
+ *
+ *  Created on: 17 avr. 2017
+ *      Author: CynthiaMiaina
+ */
+
+#include "Station.hpp"
+
+namespace std {
+
+Station::Station() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Station::~Station() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ Simulation Station Ski/scr/Station.hpp
@@ -0,0 +1,21 @@
+/*
+ * Station.hpp
+ *
+ *  Created on: 17 avr. 2017
+ *      Author: CynthiaMiaina
+ */
+
+#ifndef SCR_STATION_HPP_
+#define SCR_STATION_HPP_
+
+namespace std {
+
+class Station {
+public:
+	Station();
+	virtual ~Station();
+};
+
+} /* namespace std */
+
+#endif /* SCR_STATION_HPP_ */
