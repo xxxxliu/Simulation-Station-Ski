51f2375b64c540ae52f8452b86b35a571ee81459
CynthiaMiaina <CynthiaMiaina@cynthia-miaina>
2017-04-19 13:18:08 +0200

    Station.cpp : constructeurs

--- Simulation Station Ski/scr/Station.cpp
+++ Simulation Station Ski/scr/Station.cpp
@@ -9,10 +9,33 @@
 
 namespace std {
 
-Station::Station() {
+Station::Station(string unMode, int nbSkieurs, int d, int t) {
+	mode=unMode;
+	nombreDeSkieurs=nbSkieurs;
+	dureeOuverture=d;
+	tempsActuel=t;
 	// TODO Auto-generated constructor stub
 
 }
+
+string Station::getMode() const {
+	return mode;
+}
+
+int Station::getNombreDeSkieurs() const{
+	return nombreDeSkieurs;
+}
+
+int Station::getDureeOuverture() const {
+	return dureeOuverture;
+}
+
+int Station::getTempsActuel() const{
+	return tempsActuel;
+}
+
+
+
 
 Station::~Station() {
 	// TODO Auto-generated destructor stub
